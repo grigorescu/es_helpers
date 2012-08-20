6490d8932e2a80f19807ac1b7a67fe63af5785ca
Vlad Grigorescu <vladg@cmu.edu>
2012-08-20 16:47:43 -0400

    Install command hooks.

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     version = "0.1-r1",
     packages = ["es_helpers"],
     install_requires = ["requests==0.13.6"],
+    entry_points = {
+        'console_scripts': [
+            'es_cleanup = es_helpers.es_cleanup:delete',
+            ]
+    }
 )
