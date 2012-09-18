1fbf4112e99a4c11dafa48987be8356b81e7ad0a
Vlad Grigorescu <grigorescu@gmail.com>
2012-09-18 12:08:12 -0300

    Version bump.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "es_helpers",
-    version = "0.1-r2",
+    version = "0.1-r3",
     packages = ["es_helpers"],
     install_requires = ["requests==0.13.6"],
     entry_points = {
